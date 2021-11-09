bad72e5a15e6f47f769a2b6788a88302bfb0c277
abhizaan21 <abhi20560604@gmail.com>
2021-11-09 20:06:11 +0200

    completed test

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,89 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:flutter/services.dart';
+
+Future <List<int>> fetch() async{
+
 }
+  final String apiUrl = "https://randomuser.me/api/?results=10";
+
+  Future<List<dynamic>> fetchUsers() async {
+
+    var result = await http.get(apiUrl);
+    return json.decode(result.body)['results'];
+
+  }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+
+ static final String title= "Randomizer";
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyConverter',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'Simply Convert'),
-    );
+  Widget build(BuildContext context) =>MaterialApp(
+    debugShowCheckedModeBanner:false,
+    title:title,
+    theme:ThemeData(primarySwatch:Colors.green),
+    home: MyHomePage(),
+  );
   }
-}
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required String title}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Right(
-      child:(
 
-      )
-      )
-    )
+    // list of 5 random Number between 1 to 100
+    final list= nextNumbers(5, min: 1 , max:100);
+    print(list);
+
+    return Scaffold(
+     appBar: AppBar(
+     title: Text(MyApp.title),
+       centerTitle: true,
+     ),
+      body: Center(
+        child:Column(
+          mainAxisSize: MainAxisSize.min,
+          children:[
+            Text(
+              'Randomly Generated Number: $list',
+              style:TextStyle(fontSize: 21),
+            ),
+            Container(
+              padding:EdgeInsets.all(20.0),
+              child:ElevatedButton(
+                child: Text('Generate'),
+                onPressed: (){
+                  int nextNumber({required int min,required int max}) =>
+                    min + Random().nextInt(max - min + 1 );
+                },
+              )
+            )
+          ]
+        )
+      ),
+      );
   }
 
+ int nextNumber({required int min,required int max}) =>
+    min + Random().nextInt(max - min + 1 );
+  
+List<int> nextNumbers(int length,{required int min, required int max}){
+  final numbers= Set<int>();
+
+ while (numbers.length < length) {
+   final number= nextNumber(min: min, max: max);
+   numbers.add(number);
+ }
+ return List.of(numbers);
+  }
 }
